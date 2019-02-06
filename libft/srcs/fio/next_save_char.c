f5b4232ae6545229b9ad70fd9a7c48c465987a2f
Zexi Wang <twopieces0921@gmail.com>
2019-02-06 18:17:47 -0500

    no change

--- libft/srcs/fio/next_save_char.c
+++ libft/srcs/fio/next_save_char.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   nextchar_savechar.c                                :+:      :+:    :+:   */
+/*   next_save_clear_char.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zwang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 09:59:33 by zwang             #+#    #+#             */
-/*   Updated: 2018/10/13 13:24:42 by zwang            ###   ########.fr       */
+/*   Updated: 2019/02/06 18:12:20 by Zexi Wang        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@
 static char	g_buf[BUF_SIZ];
 static int	g_bufpos = 0;
 
-int		ft_nextchar(void)
+int			ft_nextchar(void)
 {
 	return ((g_bufpos > 0) ? g_buf[--g_bufpos] : ft_getchar());
 }
 
-void	ft_savechar(int c)
+void		ft_savechar(int c)
 {
 	if (g_bufpos >= BUF_SIZ)
 		ft_printf("ft_savechar: too many characters\n");
