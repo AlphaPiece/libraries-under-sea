511b6d96366d1df9b39d6a02e482e22b146fda68
Zexi Wang <twopieces0921@gmail.com>
2019-04-06 09:40:32 -0400

    updated ft_memrealloc

--- libft/srcs/mem/ft_memrealloc.c
+++ libft/srcs/mem/ft_memrealloc.c
@@ -6,7 +6,7 @@
 /*   By: zwang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/05 14:07:46 by zwang             #+#    #+#             */
-/*   Updated: 2018/11/04 15:35:41 by zwang            ###   ########.fr       */
+/*   Updated: 2019/04/06 09:39:44 by Zexi Wang        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@
 ** Precondition: new size must be larger than the older size
 */
 
-void	*ft_memrealloc(void **ptr, size_t size)
+void	*ft_memrealloc(void **ptr, size_t oldsize, size_t newsize)
 {
 	void	*newptr;
 
-	if (!(newptr = ft_memalloc(size)))
+	if (oldsize >= newsize)
+		return (*ptr);
+	if (!(newptr = ft_memalloc(newsize)))
 		return (NULL);
-	newptr = ft_memcpy(newptr, *ptr, size);
+	newptr = ft_memcpy(newptr, *ptr, oldsize);
 	ft_memfree(ptr);
 	return (newptr);
 }
