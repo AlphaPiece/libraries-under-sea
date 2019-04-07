43f2e59a5153c924b329ebb387549652745901f2
Zexi Wang <twopieces0921@gmail.com>
2019-04-07 12:28:01 -0400

    modified ft_randintarr

--- libft/srcs/num/ft_randintarr.c
+++ libft/srcs/num/ft_randintarr.c
@@ -6,11 +6,15 @@
 /*   By: Zexi Wang <twopieces0921@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/02 20:55:31 by Zexi Wang         #+#    #+#             */
-/*   Updated: 2019/02/02 21:51:45 by Zexi Wang        ###   ########.fr       */
+/*   Updated: 2019/04/07 12:27:12 by Zexi Wang        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Note: lower, upper are inclusive
+*/
 
 int *ft_randintarr(int lower, int upper, int len)
 {
@@ -19,10 +23,16 @@ int *ft_randintarr(int lower, int upper, int len)
 	int		i;
 	int		*arr;
 
-    range = upper - lower + 1;
-    srand((unsigned int)time(&t));
 	if (!(arr = (int *)malloc(sizeof(int) * len)))
 		return (NULL);
+	if (lower > upper)
+	{
+		i = lower;
+		lower = upper;
+		upper = i;
+	}	
+	range = upper - lower + 1;
+    srand((unsigned int)time(&t));
 	i = 0;
 	while (i < len)
 		arr[i++] = rand() % range + lower;
