a1d637d8db3ede3850c98d264210179c996cc9fb
Zexi Wang <twopieces0921@gmail.com>
2019-05-20 15:40:40 -0400

    added comment in left_rotate and right_rotate

--- ef/srcs/rbtree/ef_rbnode_left_rotate.c
+++ ef/srcs/rbtree/ef_rbnode_left_rotate.c
@@ -6,30 +6,34 @@
 /*   By: Zexi Wang <twopieces0921@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/04 10:41:37 by Zexi Wang         #+#    #+#             */
-/*   Updated: 2019/05/13 21:19:10 by Zexi Wang        ###   ########.fr       */
+/*   Updated: 2019/05/20 15:30:25 by Zexi Wang        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libef.h"
 
-void	ef_rbnode_left_rotate(t_rbtree *tree, t_rbnode *node)
-{
-	t_rbnode	*x, *y;
+/*
+** x: the node getting rotated down
+** y: x's right child which is going to rotate up to replace x
+*/
 
-	if (!tree || !node || node == tree->nil || node->right == tree->nil)
-		return ;
-	x = node;
-	y = x->right;
-	x->right = y->left;
-	if (y->left != tree->nil)
-		y->left->parent = x;
-	y->parent = x->parent;
-	if (x->parent == tree->nil)
-		tree->root = y;
-	else if (x == x->parent->left)
-		x->parent->left = y;
-	else
-		x->parent->right = y;
-	y->left = x;
-	x->parent = y;
+void	ef_rbnode_left_rotate(t_rbtree *tree, t_rbnode *x)
+{
+	t_rbnode	*y;
+
+	if (tree && x && x != tree->nil && (y = x->right) != tree->nil)
+	{
+		x->right = y->left;
+		if (y->left != tree->nil)
+			y->left->parent = x;
+		y->parent = x->parent;
+		if (x->parent == tree->nil)
+			tree->root = y;
+		else if (x == x->parent->left)
+			x->parent->left = y;
+		else
+			x->parent->right = y;
+		y->left = x;
+		x->parent = y;
+	}
 }
--- ef/srcs/rbtree/ef_rbnode_right_rotate.c
+++ ef/srcs/rbtree/ef_rbnode_right_rotate.c
@@ -6,30 +6,34 @@
 /*   By: Zexi Wang <twopieces0921@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/04 10:42:30 by Zexi Wang         #+#    #+#             */
-/*   Updated: 2019/05/13 21:18:49 by Zexi Wang        ###   ########.fr       */
+/*   Updated: 2019/05/20 15:36:00 by Zexi Wang        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libef.h"
 
-void	ef_rbnode_right_rotate(t_rbtree *tree, t_rbnode *node)
-{
-	t_rbnode	*x, *y;
+/*
+** x: the node getting rotated down
+** y: x's left child which is going to be rotated up to replace x
+*/
 
-	if (!tree || !node || node == tree->nil || node->left == tree->nil)
-		return ;
-	x = node;
-	y = x->left;
-	x->left = y->right;
-	if (y->right != tree->nil)
-		y->right->parent = x;
-	y->parent = x->parent;
-	if (x->parent == tree->nil)
-		tree->root = y;
-	else if (x == x->parent->left)
-		x->parent->left = y;
-	else
-		x->parent->right = y;
-	y->right = x;
-	x->parent = y;
+void	ef_rbnode_right_rotate(t_rbtree *tree, t_rbnode *x)
+{
+	t_rbnode	*y;
+
+	if (tree && x && x != tree->nil && (y = x->left) != tree->nil)
+	{
+		x->left = y->right;
+		if (y->right != tree->nil)
+			y->right->parent = x;
+		y->parent = x->parent;
+		if (x->parent == tree->nil)
+			tree->root = y;
+		else if (x == x->parent->left)
+			x->parent->left = y;
+		else
+			x->parent->right = y;
+		y->right = x;
+		x->parent = y;
+	}
 }
