3d2d6a4103d90d3e1374987d614d82ab2823651f
Zexi Wang <walker.wang@mail.utoronto.ca>
2020-01-26 21:10:52 -0500

    Changed back the randomized priority assignment range from 1-100 to INT_MIN-INT_MAX

--- ef/src/treap/ef_trnode_create.c
+++ ef/src/treap/ef_trnode_create.c
@@ -6,7 +6,7 @@
 /*   By: Zexi Wang <twopieces0921@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 10:09:20 by Zexi Wang         #+#    #+#             */
-/*   Updated: 2019/05/23 19:59:13 by Zexi Wang        ###   ########.fr       */
+/*   Updated: 2020/01/26 21:09:14 by Zexi Wang        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ t_trnode	*ef_trnode_create(void *key, void *value)
 	node->parent = NULL;
 	node->left = NULL;
 	node->right = NULL;
-//	node->priority = ft_randint(INT_MIN + 1, INT_MAX);
-	node->priority = ft_randint(0, 100);
+	node->priority = ft_randint(INT_MIN + 1, INT_MAX);
 	return (node);
 }
